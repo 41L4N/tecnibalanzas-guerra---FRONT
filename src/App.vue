b317792183620e7556f5f680b97711ee6fdfb1a2
Ailan Ñañez <ailanruiz1994@gmail.comz>
2026-05-15 05:05:25 -0400

    Update dependencies in package.json and package-lock.json to include PrimeVue and PrimeUIX themes, and configure PrimeVue in main.ts for theme support. Refactor App.vue to maintain script setup structure.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
-
 <template>
   <router-view />
+  <Button label="Click" />
 </template>
+
+<script setup lang="ts">
+import Button from "primevue/button"
+</script>
 
 <style scoped></style>
